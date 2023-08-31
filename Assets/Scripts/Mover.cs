f083558c7ff2faa3196ea2e99536f468f8f273e5
sagentoft <soren.agentoft@gmail.com>
2023-08-31 21:20:37 +0200

    Separated the control layer

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -11,29 +11,13 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        
-
-        if (Input.GetMouseButton(0))
-        {
-            MoveToCursor();
-        }
-
         UpdateAnimator();
-
     }
-    
-    private void MoveToCursor() 
-    { 
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
 
-        bool hasHit = Physics.Raycast(ray, out hit);
 
-        if (hasHit)
-        {
-            GetComponent<NavMeshAgent>().destination = hit.point;
-        }
-        
+    public void MoveTo(Vector3 destination)
+    {
+        GetComponent<NavMeshAgent>().destination = destination;
     }
 
     private void UpdateAnimator()
